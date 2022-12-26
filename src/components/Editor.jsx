14fa467b34cf0953b906860c618db60970e415db
imadityaanand <103442460+imadityaanand@users.noreply.github.com>
2022-12-26 12:57:17 +0530

    Add initial files.

---
+++ src/components/Editor.jsx
@@ -0,0 +1,34 @@
+import React, { Component } from 'react'
+import "./Editor.css"
+
+export default class Editor extends Component {
+    constructor(props) {
+        super(props);
+        // to bind the method with the event handler without (). 
+        this.handleChange = this.handleChange.bind(this);
+
+        this.state =
+         { 
+            value: ''
+        };
+    }
+
+    handleChange(e){
+        this.setState({value: e.target.value});
+    }
+
+    render() {
+        return (
+            <div className="container">
+                <div className="input">
+                    <h3>Input</h3>
+                    <textarea onChange={this.handleChange} className="input-text"/>
+                </div>
+                <div className="output">
+                    <h3>Pro Note</h3>
+                    <div className="output-text">{this.state.value}</div>
+                </div>                
+            </div>
+        )
+    }
+}
